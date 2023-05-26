77303abeae625a3dbc56b14eb1af7e622fa3284a
qianL93 <meliuqian@gmail.com>
2023-05-26 14:15:44 +0000

    fix: localStorage is not defined

--- src/service/localStorage.ts
+++ src/service/localStorage.ts
@@ -11,15 +11,20 @@ interface LocalStorage<T> {
 const storage = <T>(): LocalStorage<T> => ({
     set: (key: string, value: T) => {
         try {
-            localStorage.setItem(key, JSON.stringify(value));
+            if (typeof window !== 'undefined') {
+                localStorage.setItem(key, JSON.stringify(value));
+            }
         } catch (error) {
             console.error("Error saving to localStorage:", error);
         }
     },
     get: (key: string) => {
         try {
-            const item = localStorage.getItem(key);
-            return item ? JSON.parse(item) : null;
+            if (typeof window !== 'undefined') {
+                const item = localStorage.getItem(key);
+                return item ? JSON.parse(item) : null;
+            }
+            return null;
         } catch (error) {
             console.error("Error getting from localStorage:", error);
             return null;
